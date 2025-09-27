77bff203ecd0f35bad947869517ba6100f5d57d9
sujithreddy123 <139841876+sujithreddy123@users.noreply.github.com>
2025-09-27 16:02:29 +0530

    Update create_user_table.sql

--- create_user_table.sql
+++ create_user_table.sql
@@ -1,20 +1,16 @@
 --
 -- Table structure for table `users`
 --
+CREATE TABLE Venue (
+    venue_id INT PRIMARY KEY AUTO_INCREMENT,
+    venue_name VARCHAR(100) NOT NULL,
+    location VARCHAR(150),
+    capacity INT
+);
 
-CREATE TABLE IF NOT EXISTS `users` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `username` varchar(50) NOT NULL,
-  `password` varchar(32) NOT NULL,
-  `role` varchar(10) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=3 ;
 
 --
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`id`, `username`, `password`, `role`) VALUES
-(1, 'joe@bloggs.com', 'secret', 'admin'),
-(2, 'mary@bloggs.com', 'secret', 'staff'),
-(3, 'fred@bloggs.com', 'secret', 'user');
+
